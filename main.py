d710974bf73697c84f7a5890f677f01328af272b
dy0787 <79344352+dy0787@users.noreply.github.com>
2023-09-03 15:09:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import streamlit as st
 from sum1 import nltk_summarizer
+from sum4 import generate_summary
 
 st.title("Text summarixer")
-menu = ["Home","TextS1","TextS2"]
+menu = ["Home","Extractive","Abstractive"]
 choice = st.sidebar.selectbox("Menu",menu)
 
 if choice == "Home":
         st.subheader("Home")
+        st.write("AI mini project")
 
-elif choice == "TextS1":
-        st.subheader("TextS1")
-        input=st.text_input("Input text")
+elif choice == "Extractive":
+        st.subheader("Extractive")
+        input=st.text_area("Input text")
         if st.button("Generate"):
                 st.write(nltk_summarizer(input))
 
-elif choice == "TextS2":
-        st.subheader("TextS2")
+elif choice == "Abstractive":
+        st.subheader("Abstractive")
+        input=st.text_area("Input text")
+        mi = st.slider('minimum number of words', 40, 200, 25)
+        ma = st.slider('maximum number of words', 40, 200, 25)
+        if st.button("Generate"):
+
+                st.write(generate_summary(input,mi,ma))
+
+
